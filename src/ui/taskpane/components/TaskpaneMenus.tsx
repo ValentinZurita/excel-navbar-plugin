7bc7601bc9318256aab84a703068dc851288531d
ValentinZurita <67253631+ValentinZurita@users.noreply.github.com>
2026-04-11 12:30:02 -0600

    refactor(navigation): separate taskpane menu actions

--- src/ui/taskpane/components/TaskpaneMenus.tsx
+++ src/ui/taskpane/components/TaskpaneMenus.tsx
@@ -13,6 +13,13 @@ import {
 } from '../../icons';
 import '../styles/TaskpaneMenus.css';
 
+interface MenuAction {
+  key: string;
+  icon: ReactNode;
+  label: string;
+  onSelect: () => void;
+}
+
 interface TaskpaneMenusProps {
   activeMenu: ContextMenuState | null;
   onCloseMenus: () => void;
@@ -45,6 +52,12 @@ function getMenuStyle(menu: SheetMenuState | GroupMenuState) {
   };
 }
 
+function renderMenuActions(actions: MenuAction[]) {
+  return actions.map((action) => (
+    <MenuItem key={action.key} icon={action.icon} label={action.label} onClick={action.onSelect} />
+  ));
+}
+
 function ContextMenuLayer({ children, menu, onCloseMenus }: { children: ReactNode; menu: ContextMenuState; onCloseMenus: () => void }) {
   return (
     <div
@@ -67,6 +80,107 @@ function ContextMenuLayer({ children, menu, onCloseMenus }: { children: ReactNod
   );
 }
 
+function buildSheetMenuActions(
+  sheetMenu: SheetMenuState,
+  handlers: Pick<
+    TaskpaneMenusProps,
+    'onCloseMenus' | 'onTogglePin' | 'onToggleVisibility' | 'onRenameWorksheet' | 'onRemoveFromGroup' | 'onCreateGroup'
+  >,
+): MenuAction[] {
+  const actions: MenuAction[] = [
+    {
+      key: 'toggle-pin',
+      icon: sheetMenu.worksheet.isPinned ? (
+        <PinOffMenuIcon className="context-menu-icon-svg" />
+      ) : (
+        <PinMenuIcon className="context-menu-icon-svg" />
+      ),
+      label: sheetMenu.worksheet.isPinned ? 'Unpin tab' : 'Pin tab',
+      onSelect: () => {
+        handlers.onTogglePin(sheetMenu.worksheet);
+        handlers.onCloseMenus();
+      },
+    },
+    {
+      key: 'toggle-visibility',
+      icon: sheetMenu.worksheet.visibility === 'Visible' ? (
+        <EyeOffIcon className="context-menu-icon-svg" />
+      ) : (
+        <EyeIcon className="context-menu-icon-svg" />
+      ),
+      label: sheetMenu.worksheet.visibility === 'Visible' ? 'Hide sheet' : 'Unhide sheet',
+      onSelect: () => {
+        handlers.onToggleVisibility(sheetMenu.worksheet);
+        handlers.onCloseMenus();
+      },
+    },
+    {
+      key: 'rename',
+      icon: <RenameMenuIcon className="context-menu-icon-svg" />,
+      label: 'Rename',
+      onSelect: () => {
+        handlers.onRenameWorksheet(sheetMenu.worksheet);
+      },
+    },
+  ];
+
+  if (sheetMenu.worksheet.groupId) {
+    actions.push({
+      key: 'remove-from-group',
+      icon: <RemoveMenuIcon className="context-menu-icon-svg" />,
+      label: 'Remove from group',
+      onSelect: () => {
+        handlers.onRemoveFromGroup(sheetMenu.worksheet.worksheetId);
+        handlers.onCloseMenus();
+      },
+    });
+  }
+
+  actions.push({
+    key: 'new-group',
+    icon: <AddGroupMenuIcon className="context-menu-icon-svg" />,
+    label: 'New group',
+    onSelect: () => {
+      handlers.onCreateGroup(sheetMenu.worksheet.worksheetId);
+    },
+  });
+
+  return actions;
+}
+
+function buildGroupMenuActions(
+  groupMenu: GroupMenuState,
+  handlers: Pick<TaskpaneMenusProps, 'onCloseMenus' | 'onCreateGroup' | 'onRenameGroup' | 'onDeleteGroup'>,
+): MenuAction[] {
+  return [
+    {
+      key: 'rename-group',
+      icon: <RenameMenuIcon className="context-menu-icon-svg" />,
+      label: 'Rename group',
+      onSelect: () => {
+        handlers.onRenameGroup(groupMenu.groupId, groupMenu.groupName);
+      },
+    },
+    {
+      key: 'delete-group',
+      icon: <DeleteMenuIcon className="context-menu-icon-svg" />,
+      label: 'Delete group',
+      onSelect: () => {
+        handlers.onDeleteGroup(groupMenu.groupId, groupMenu.groupName);
+        handlers.onCloseMenus();
+      },
+    },
+    {
+      key: 'new-group',
+      icon: <AddGroupMenuIcon className="context-menu-icon-svg" />,
+      label: 'New group',
+      onSelect: () => {
+        handlers.onCreateGroup();
+      },
+    },
+  ];
+}
+
 function SheetContextMenu({
   sheetMenu,
   onCloseMenus,
@@ -84,62 +198,16 @@ function SheetContextMenu({
   onRemoveFromGroup: (worksheetId: string) => void;
   onCreateGroup: (initialWorksheetId?: string) => void;
 }) {
-  return (
-    <ContextMenuLayer menu={sheetMenu} onCloseMenus={onCloseMenus}>
-      <MenuItem
-        icon={
-          sheetMenu.worksheet.isPinned ? (
-            <PinOffMenuIcon className="context-menu-icon-svg" />
-          ) : (
-            <PinMenuIcon className="context-menu-icon-svg" />
-          )
-        }
-        label={sheetMenu.worksheet.isPinned ? 'Unpin tab' : 'Pin tab'}
-        onClick={() => {
-          onTogglePin(sheetMenu.worksheet);
-          onCloseMenus();
-        }}
-      />
-      <MenuItem
-        icon={
-          sheetMenu.worksheet.visibility === 'Visible' ? (
-            <EyeOffIcon className="context-menu-icon-svg" />
-          ) : (
-            <EyeIcon className="context-menu-icon-svg" />
-          )
-        }
-        label={sheetMenu.worksheet.visibility === 'Visible' ? 'Hide sheet' : 'Unhide sheet'}
-        onClick={() => {
-          onToggleVisibility(sheetMenu.worksheet);
-          onCloseMenus();
-        }}
-      />
-      <MenuItem
-        icon={<RenameMenuIcon className="context-menu-icon-svg" />}
-        label="Rename"
-        onClick={() => {
-          onRenameWorksheet(sheetMenu.worksheet);
-        }}
-      />
-      {sheetMenu.worksheet.groupId ? (
-        <MenuItem
-          icon={<RemoveMenuIcon className="context-menu-icon-svg" />}
-          label="Remove from group"
-          onClick={() => {
-            onRemoveFromGroup(sheetMenu.worksheet.worksheetId);
-            onCloseMenus();
-          }}
-        />
-      ) : null}
-      <MenuItem
-        icon={<AddGroupMenuIcon className="context-menu-icon-svg" />}
-        label="New group"
-        onClick={() => {
-          onCreateGroup(sheetMenu.worksheet.worksheetId);
-        }}
-      />
-    </ContextMenuLayer>
-  );
+  const actions = buildSheetMenuActions(sheetMenu, {
+    onCloseMenus,
+    onTogglePin,
+    onToggleVisibility,
+    onRenameWorksheet,
+    onRemoveFromGroup,
+    onCreateGroup,
+  });
+
+  return <ContextMenuLayer menu={sheetMenu} onCloseMenus={onCloseMenus}>{renderMenuActions(actions)}</ContextMenuLayer>;
 }
 
 function GroupContextMenu({
@@ -155,32 +223,14 @@ function GroupContextMenu({
   onRenameGroup: (groupId: string, groupName: string) => void;
   onDeleteGroup: (groupId: string, groupName: string) => void;
 }) {
-  return (
-    <ContextMenuLayer menu={groupMenu} onCloseMenus={onCloseMenus}>
-      <MenuItem
-        icon={<RenameMenuIcon className="context-menu-icon-svg" />}
-        label="Rename group"
-        onClick={() => {
-          onRenameGroup(groupMenu.groupId, groupMenu.groupName);
-        }}
-      />
-      <MenuItem
-        icon={<DeleteMenuIcon className="context-menu-icon-svg" />}
-        label="Delete group"
-        onClick={() => {
-          onDeleteGroup(groupMenu.groupId, groupMenu.groupName);
-          onCloseMenus();
-        }}
-      />
-      <MenuItem
-        icon={<AddGroupMenuIcon className="context-menu-icon-svg" />}
-        label="New group"
-        onClick={() => {
-          onCreateGroup();
-        }}
-      />
-    </ContextMenuLayer>
-  );
+  const actions = buildGroupMenuActions(groupMenu, {
+    onCloseMenus,
+    onCreateGroup,
+    onRenameGroup,
+    onDeleteGroup,
+  });
+
+  return <ContextMenuLayer menu={groupMenu} onCloseMenus={onCloseMenus}>{renderMenuActions(actions)}</ContextMenuLayer>;
 }
 
 export function TaskpaneMenus({
